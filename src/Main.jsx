3233105c3e553fa20e820580ed3d02d166d6085f
stm <stm2485@gmail.com>
2023-03-12 10:55:00 +0200

    optimized it

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 export default function Main(){
   return(
-    <div className="main-section">
+    <section className="main-section">
       <img src="photo-grid.png" alt="photo" className="img-section" />
       <h1 className="heading">Online experiences</h1>
       <p className="content">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
       </p>
-    </div>
+    </section>
   )
 }
