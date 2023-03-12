4e128bf1b25ffdcf6124c4d96bda2952058fc019
stm <stm2485@gmail.com>
2023-03-12 10:44:03 +0200

    Added nav+Main

---
+++ src/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Navbar(){
+  return(
+    <nav className="nav">
+    <img src="airbnb-logo.png" alt="airbnb" className="logo" />
+    </nav>
+  )
+}
